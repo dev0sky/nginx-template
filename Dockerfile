ad2d1260e92aac64243da5498a5e894f6621c5e5
Arturo Sáenz <jorgesaenzsanchez@mediaaerea.net>
2024-12-17 22:32:13 -0600

    Updated Nginx for template integrity

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM nginx:1.27.3
+WORKDIR /
 COPY ./conf.d/local.conf.template /etc/nginx/conf.d/local.conf.template
 COPY ./entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${NGINX_APP_PORT} ${PROTOCOL} ${MAIN_DOMAIN} ${DJANGO_APP_PORT} ${REDIS_HOST} ${REDIS_PORT}' < /etc/nginx/conf.d/local.conf.template > /etc/nginx/conf.d/local.conf
+envsubst '${NGINX_APP_PORT} ${PROTOCOL} ${MAIN_DOMAIN} ${DJANGO_APP_PORT} ${REDIS_HOST} ${REDIS_PORT} ${ENCODING}' < /etc/nginx/conf.d/local.conf.template > /etc/nginx/conf.d/local.conf
 exec nginx -g 'daemon off;'
